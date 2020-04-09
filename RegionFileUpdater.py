b3a4351f7e0d4d981f0b8a50e404044dcd319fd0
Fallen_Breath <yqx791125833@gmail.com>
2020-04-09 17:21:35 +0800

    linux / windows support

--- RegionFileUpdater.py
+++ RegionFileUpdater.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import copy
 import os
+import shutil
 from imp import load_source
 import time
 try:
@@ -9,7 +10,7 @@ except ImportError:
 	import queue as Queue
 
 
-SourceWorldPath = '/home/survival-25501/quick/world/'
+SourceWorldPath = 'D:\\MCDReforged\\qb_multi\\slot1\\world\\'
 DestinationWorldPath = 'server/world/'
 Prefix = '!!region'
 PluginName = 'RegionFileUpdater'
@@ -17,22 +18,23 @@ LogFileFolder = 'log/'
 LogFilePath = LogFileFolder + PluginName + '.log'
 Debug_output = 0
 DimensionRegionFolder = {-1: 'DIM-1/region/', 0: 'region/', 1: 'DIM1/region/'}
-HelpMessage = '''------MCD ''' + PluginName + ''' v1.0------
+HelpMessage = '''
+------MCD ''' + PluginName + ''' v1.1------
 一个更新本服区域文件至生存服!!qb存档区域文件的插件
 §a【指令说明】§r
-§7''' + Prefix + ''' §r显示帮助信息
-§7''' + Prefix + ''' add §r添加玩家所在位置的区域文件
-§7''' + Prefix + ''' add §6[d] [x] [z] §r添加指定的区域文件
-§7''' + Prefix + ''' del §r删除玩家所在位置的区域文件
-§7''' + Prefix + ''' del §6[d] [x] [z] §r删除指定的区域文件
-§7''' + Prefix + ''' delete-all §r删除所有区域文件
-§7''' + Prefix + ''' list §r列出待更新的区域文件
-§7''' + Prefix + ''' history §r输出上一次update的结果
-§7''' + Prefix + ''' update §r更新列表中的区域文件，这将重启服务器
+§7{0} §r显示帮助信息
+§7{0} add §r添加玩家所在位置的区域文件
+§7{0} add §6[d] [x] [z] §r添加指定的区域文件
+§7{0} del §r删除玩家所在位置的区域文件
+§7{0} del §6[d] [x] [z] §r删除指定的区域文件
+§7{0} delete-all §r删除所有区域文件
+§7{0} list §r列出待更新的区域文件
+§7{0} history §r输出上一次update的结果
+§7{0} update §r更新列表中的区域文件，这将重启服务器
 §a【参数说明】§r
 §6[d]§r: 维度序号，主世界为0，下界为-1，末地为1
 §6[x] [z]§r: 区域文件坐标，如r.-3.1.mca的区域文件坐标为x=-3 z=1
-'''
+'''.strip().format(Prefix)
 
 regionList = []
 historyList = []
@@ -57,6 +59,7 @@ def printLog(msg):
 			pass
 	with open(LogFilePath, 'a') as logfile:
 		logfile.write(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())) + ': ' + msg + '\n')
+	print(msg)
 
 
 def getPlayerInfo(server, name, path=''):
@@ -67,10 +70,12 @@ def getPlayerInfo(server, name, path=''):
 	return api.getPlayerInfo(server, name, path)
 
 
+def getRegionFileName(regionfile):
+	return 'r.' + str(regionfile[1]) + '.' + str(regionfile[2]) + '.mca'
+
+
 def getRegionFilePath(regionfile):
-	path = ''
-	path += DimensionRegionFolder[regionfile[0]]
-	path += 'r.' + str(regionfile[1]) + '.' + str(regionfile[2]) + '.mca'
+	path = os.path.join(DimensionRegionFolder[regionfile[0]], getRegionFileName(regionfile))
 	return path
 
 
@@ -80,7 +85,7 @@ def addRegion(server, info, regionFile):
 		printMessage(server, info, '列表中已存在该区域文件')
 	else:
 		regionList.append(regionFile)
-		printMessage(server, info, '区域文件§6' + getRegionFilePath(regionFile) + '§r已添加')
+		printMessage(server, info, '区域文件§6{}§r已添加'.format(getRegionFilePath(regionFile)))
 
 
 def delRegion(server, info, regionFile):
@@ -89,7 +94,7 @@ def delRegion(server, info, regionFile):
 		printMessage(server, info, '列表中不存在该区域文件')
 	else:
 		regionList.remove(regionFile)
-		printMessage(server, info, '区域文件§6' + getRegionFilePath(regionFile) + '§r已删除')
+		printMessage(server, info, '区域文件§6{}§r已删除'.format(getRegionFilePath(regionFile)))
 
 
 def deleteRegionList(server, info):
@@ -116,17 +121,17 @@ def getRegionFileFromParameter(p):
 
 def printRegionList(server, info):
 	global regionList
-	printMessage(server, info, '更新列表中共有' + str(len(regionList)) + '个待更新的区域文件')
+	printMessage(server, info, '更新列表中共有{}个待更新的区域文件'.format(len(regionList)))
 	for region in regionList:
-		printMessage(server, info, '§6' + getRegionFilePath(region) + '§r')
+		printMessage(server, info, '§6{}§r'.format(getRegionFilePath(region)))
 
 
 def printRegionHistory(server, info):
 	global historyList
-	printMessage(server, info, '上次尝试更新更新了' + str(len(historyList)) + '个区域文件')
+	printMessage(server, info, '上次尝试更新更新了{}个区域文件'.format(len(historyList)))
 	msg = {False: '失败', True: '成功'}
 	for history in historyList:
-		printMessage(server, info, '§6' + getRegionFilePath(history[0]) + '§r: ' + msg[history[1]])
+		printMessage(server, info, '§6{}§r: {}'.format(getRegionFilePath(history[0]), msg[history[1]]))
 
 
 def updateRegionFile(server, info):
@@ -134,12 +139,17 @@ def updateRegionFile(server, info):
 	global historyList
 	printRegionList(server, info)
 	countdown = 5
-	printMessage(server, info, '[' + PluginName + ']: ' + str(countdown) + '秒后重启服务器更新列表中的区域文件')
+	printMessage(server, info, '[{}]: {}秒后重启服务器更新列表中的区域文件'.format(PluginName, countdown), isBroadcast=True)
 	for i in range(1, countdown):
-		printMessage(server, info, '[' + PluginName + ']: 还有' + str(countdown - i) + '秒', isBroadcast=True)
+		printMessage(server, info, '[{}]: 还有{}秒'.format(PluginName, countdown - i), isBroadcast=True)
 		time.sleep(1)
+
 	server.stop()
-	time.sleep(10)
+	if hasattr(server, 'MCDR'):
+		server.wait_for_start()
+	else:
+		time.sleep(10)
+
 	if info.isPlayer:
 		name = info.player
 	else:
@@ -147,18 +157,22 @@ def updateRegionFile(server, info):
 	printLog(name + '更新了' + str(len(regionList)) + '个区域文件：')
 	historyList = []
 	for region in regionList:
-		sourceFilePath = SourceWorldPath + getRegionFilePath(region)
-		DestinationFilePath = DestinationWorldPath + DimensionRegionFolder[region[0]]
-		flag = False
-		if (os.path.isfile(sourceFilePath)):
-			print('[' + PluginName + ']cp -f ' + sourceFilePath + ' ' + DestinationFilePath)
-			os.system('cp -f ' + sourceFilePath + ' ' + DestinationFilePath)
+		sourceFilePath = os.path.join(SourceWorldPath, getRegionFilePath(region))
+		destinationFilePath = os.path.join(DestinationWorldPath, DimensionRegionFolder[region[0]], getRegionFileName(region))
+		try:
+			print('[{}] {} -> {}'.format(PluginName, sourceFilePath, destinationFilePath))
+			shutil.copyfile(sourceFilePath, destinationFilePath)
+		except Exception as e:
+			msg = '失败，错误信息：{}'.format(str(e))
+			flag = False
+		else:
+			msg = '成功'
 			flag = True
 		historyList.append((region, flag))
-		msg = {False: '失败', True: '成功'}
-		printLog(getRegionFilePath(region) + ': ' + msg[flag])
+		printLog(getRegionFilePath(region) + ': ' + msg)
+
 	regionList = []
-	time.sleep(5)
+	time.sleep(1)
 	server.start()
 
 
